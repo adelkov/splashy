dd4a96e7679a945368b884ab5ae38077e9fccadc
Kovács Adél <kovacsadel@Kovacss-MacBook-Pro-2.local>
2019-09-22 10:05:45 +0200

    refact: images divider function

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,23 +1,18 @@
 export const divideImagesToColumns = images => {
-  const len = images.length;
-  
-  if (len === 1) {
-    return [[], [images[0]], []];
-  }
-
-  if (len === 2) {
-    return [[images[0]], [images[1]], []];
-  }
-
-  if (len === 3) {
-    return [[images[0]], [images[1]], [images[2]]];
-  }
-  
-  const chunk = Math.floor(len / 3);
-  const col1 = images.slice(0, chunk);
-  const col2 = images.slice(chunk, len - chunk);
-  const col3 = images.slice(len - chunk, len);
-  return [col1, col2, col3];
+  const columns = images.reduce(
+    (acc, curr, idx) => {
+      if (idx % 3 === 0) {
+        acc[0].push(curr);
+      } else if (idx % 3 === 1) {
+        acc[1].push(curr);
+      } else {
+        acc[2].push(curr);
+      }
+      return acc;
+    },
+    [[], [], []]
+  );
+  return columns;
 };
 
 export const createUrl = (params = {}, url = "") => {
